fd1e1828b12994f2b0c863f59fb1a2cb4d5933d8
marcmarfer <marcmf2004@gmail.com>
2026-09-25 12:38:52 +0200

    Add PWA and home screen icons

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
       manifest: {
         name: 'SetLift',
         short_name: 'SetLift',
